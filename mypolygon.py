7bf8160d02b9c37eb161a3b8526f015158424abe
Zheng Fu <zf38@cornell.edu>
2018-07-10 20:29:29 -0500

    finish implementing polygon.py and start working on flower.py

--- mypolygon.py
+++ mypolygon.py
@@ -2,31 +2,47 @@ import turtle
 import math
 
 
-def square(t, length):
-    for i in range(4):
-        t.fd(length)
-        t.rt(90)
-
-
-def polygon(t, n, length):
-    angle = 360/n
+def polyline(t, n, length, angle):
+    """Draws n line segmments with the given length and
+    angle (in degrees) between them. t is a turtle.
+    """
     for i in range(n):
         t.fd(length)
         t.rt(angle)
 
 
-def circle(t, r):
-    circumference = 2 * math.pi * r
-    n = 720
-    length = circumference / n
-    polygon(t, n, length)
+def polygon(t, n, length):
+    angle = 360/n
+    polyline(t, n, length, angle)
 
 
 def arc(t, r, angle):
-    for i in range(angle):
-        t.fd(2*math.pi*r/360)
-        t.rt(1)
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    t.lt(step_angle / 2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
 
 
-bob = turtle.Turtle()
-circle(bob, r=50)
+def circle(t, r):
+    arc(t, r, 360)
+
+
+def square(t, length):
+    polygon(t, 4, length)
+
+
+if __name__ == '__main__':
+    bob = turtle.Turtle()
+
+    radius = 100
+    bob.pu()
+    bob.fd(radius)
+    bob.lt(90)
+    bob.pd()
+    circle(bob, radius)
+
+    turtle.mainloop()
