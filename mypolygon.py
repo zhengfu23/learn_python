84f94b836efe95d8adf09016b3ca082dde56abe0
Zheng Fu <zf38@cornell.edu>
2018-07-10 21:28:19 -0500

    finished implementing flower.py

--- mypolygon.py
+++ mypolygon.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division
+
 import turtle
 import math
 
@@ -8,16 +10,16 @@ def polyline(t, n, length, angle):
     """
     for i in range(n):
         t.fd(length)
-        t.rt(angle)
+        t.lt(angle)
 
 
 def polygon(t, n, length):
-    angle = 360/n
+    angle = 360.0/n
     polyline(t, n, length, angle)
 
 
 def arc(t, r, angle):
-    arc_length = 2 * math.pi * r * angle / 360
+    arc_length = 2 * math.pi * r * abs(angle) / 360
     n = int(arc_length / 4) + 3
     step_length = arc_length / n
     step_angle = float(angle) / n
